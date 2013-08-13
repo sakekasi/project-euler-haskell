26d3391f3b3bbc442a888089ecc222ed303e62e3
Saketh Kasibatla <sake.kasi@gmail.com>
2013-08-12 17:11:07 -0700

    simplified program
    * changed <code>(\x y -> x + y)</code> to <code>(+)</code>
    * added .hs to filename in comments

--- problem-1.hs
+++ problem-1.hs
@@ -1,6 +1,6 @@
- -- problem-1
+ -- problem-1.hs
  -- Find the sum of all the multiples of 3 or 5 below 1000.
 
 
  main :: IO()
- main = print $ foldr (\x y -> x + y) 0 [x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0]
+ main = print $ foldr (+) 0 [x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0]
--- problem-2.hs
+++ problem-2.hs
@@ -1,10 +1,10 @@
---problem-1
+--problem-2.hs
 --By considering the terms in the Fibonacci sequence whose values do not 
 --exceed four million, find the sum of the even-valued terms.
 
 
 main :: IO()
-main = print $ foldr (\x y -> x + y) 0 even_fibs
+main = print $ foldr (+) 0 even_fibs
 
 fibs = map fst $ iterate (\(a,b) -> (b,a+b)) (0,1)
 even_fibs = filter (\x -> even x) $ takeWhile (\x -> x < 4000000) fibs
