205b41db0072f0533a43f55a98627392374bc226
Saketh Kasibatla <sake.kasi@gmail.com>
2013-08-12 17:06:27 -0700

    modified problem 3
    avoids monads

--- problem-3.hs
+++ problem-3.hs
@@ -5,10 +5,8 @@ main :: IO()
 main = print $ foldr (\x y -> if x > y then x else y) 0 $ filter prime $ factors 600851475143
 
 factors :: Integer -> [Integer]
-factors n = do
-	let a = [ x | x <- [2 .. (ceiling . sqrt $ fromIntegral n) ], n `mod` x == 0 ]
-	let b = [ n `div` x | x <- a]
-	b ++ a
+factors n = [ n `div` x | x <- a] ++ a
+	where a = [ x | x <- [2 .. (ceiling . sqrt $ fromIntegral n) ], n `mod` x == 0 ]
 
 prime :: Integer -> Bool
 prime n = length (factors n) == 0
